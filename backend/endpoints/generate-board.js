e0fe9b423ecbe72c8498b831e89e1cc0df6a5648
Ben Stanfield <ben@skipper.ai>
2020-01-12 10:59:43 -0800

    successfully gens both boards

--- backend/endpoints/generate-board.js
+++ backend/endpoints/generate-board.js
@@ -5,23 +5,25 @@ const { matchPassword, getRandomWords } = require('../queries');
 // 1 = correct (9)
 // 2 = assassin (3)
 
-const getRandomNum = (max, min) => {
-  console.log('max: ', max);
-  console.log('min: ', min);
-  const rand = Math.floor(Math.random() * (max - min + 1) + min);
-  console.log('RAND: ', rand);
-  return rand;
-};
+const getRandomNum = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
 
 const countOccurences = (max, number, arr) => R.equals(
   max,
   R.reduce((i, num) => num === number ? i + 1 : i, 0, arr),
 );
 
-const addTile = (arr) => {
+const addTile = R.curry((arr, overlap) => {
   const neutral = countOccurences(13, 0, arr);
-  const correct = countOccurences(9, 1, arr);
+  const correct = countOccurences(overlap ? 6 : 9, 1, arr); // only add 6 more to account for overlap
   const assassin = countOccurences(3, 2, arr);
+
+  if (overlap) {
+    if (R.any(
+      (overlap) => overlap === arr.length
+    )(overlap)) {
+      return R.append(1, arr);
+    }
+  }
 
   if (assassin && correct) {
     return R.append(0, arr);
@@ -38,7 +40,7 @@ const addTile = (arr) => {
   } else {
     return R.append(getRandomNum(2, 0), arr);
   }
-}
+});
 
 const getAllIndexes = (arr, val) => {
   let indexes = [], i = -1;
@@ -48,18 +50,20 @@ const getAllIndexes = (arr, val) => {
   return indexes;
 }
 
-const generatePlayerOneKeyCard = words => R.reduce(addTile, [], words);
+const generatePlayerKeyCard = (words, overlap) => R.reduce(addTile(R.__, overlap), [], words);
 
 const generatePlayerTwoKeyCard = (p1, words) => {
   // maybe use indexOf to find location of occurences of correct?
   let allIndexes = getAllIndexes(p1, 1);
   const r1 = allIndexes[getRandomNum(allIndexes.length - 1, 0)];
-  allIndexes = R.without([random1], allIndexes);
+  allIndexes = R.without([r1], allIndexes);
   const r2 = allIndexes[getRandomNum(allIndexes.length - 1, 0)];
-  allIndexes = R.without([random2], allIndexes);
+  allIndexes = R.without([r2], allIndexes);
   const r3 = allIndexes[getRandomNum(allIndexes.length - 1, 0)];
   const overlappingIndexes = [r1, r2, r3];
-  // map over and create board. Make sure those indexes are correct (only fill 6 trues)
+
+  const playerTwoBoard = generatePlayerKeyCard(words, overlappingIndexes);
+  return playerTwoBoard;
 };
 
 exports.generateBoard = async (req, res) => {
@@ -76,12 +80,12 @@ exports.generateBoard = async (req, res) => {
 
   const wordsObjs = await getRandomWords(numberOfWords || 25);
   const wordsArr = R.pluck('name', wordsObjs.rows);
-  const playerOne = generatePlayerOneKeyCard(wordsArr);
-  const PlayerTwo = generatePlayerTwoKeyCard(playerOne, wordsArr);
+  const playerOne = generatePlayerKeyCard(wordsArr);
+  const playerTwo = generatePlayerTwoKeyCard(playerOne, wordsArr);
 
   res.status(200).send({ 
     words: wordsArr,
     playerOne,
-    // player2,
+    playerTwo,
   });
 };
