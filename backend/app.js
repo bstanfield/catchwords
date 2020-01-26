03034357e77044ada71f273390f7a460bd2aaeef
bstanfield <bnstnfld@gmail.com>
2020-01-26 15:57:07 -0800

    minor tweak

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Standard messages
 app.get('/', (req, res) => {
-    res.json({ info: 'Hello world 👩‍💻👨‍💻' })
+    res.json({ info: 'Hello world!' })
 });
 
 // Get words
