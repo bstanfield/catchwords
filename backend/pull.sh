fe8a3fa81c87a0cb14631aaaf88fffd7cbfe28a9
root <root@catchwords-instance.us-west2-a.c.catchword.internal>
2020-01-27 00:02:25 +0000

    added path for pkill

--- backend/pull.sh
+++ backend/pull.sh
@@ -1,6 +1,6 @@
 sudo git add .
 sudo git stash
 sudo git pull
-sudo pkill -9 node
+sudo /usr/share/man/man1/pkill.1.gz -9 node
 sudo /home/ben/.nvm/versions/node/v13.7.0/bin/node app.js &
 echo 'Running...'
