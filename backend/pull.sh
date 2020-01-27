6131bea68bd003b188126fe82c489c364d17c63c
root <root@catchwords-instance.us-west2-a.c.catchword.internal>
2020-01-27 00:05:00 +0000

    just killing it

--- backend/pull.sh
+++ backend/pull.sh
@@ -1,6 +1,7 @@
 sudo git add .
 sudo git stash
 sudo git pull
-sudo /usr/share/man/man1/pkill.1.gz -9 node
-sudo /home/ben/.nvm/versions/node/v13.7.0/bin/node app.js &
-echo 'Running...'
+echo 'Killing node'
+sudo pkill -9 node
+# sudo /home/ben/.nvm/versions/node/v13.7.0/bin/node app.js &
+# echo 'Running...'
