35057e143de79c00fd72b66f9e38bea421b654a2
bstanfield <bnstnfld@gmail.com>
2020-02-16 14:19:55 -0800

    Updates

---
+++ precache-manifest.db20395f94e7d5f3204b42bb7d42c39a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981ae5d985a22bb5a47346751dc972b7",
+    "url": "/catchwords/index.html"
+  },
+  {
+    "revision": "2f5767feae076d22c405",
+    "url": "/catchwords/static/js/2.697d4df4.chunk.js"
+  },
+  {
+    "revision": "2eb78f624d97a5c00de0",
+    "url": "/catchwords/static/js/main.b193749f.chunk.js"
+  },
+  {
+    "revision": "30f8782ecc5be20eeff7",
+    "url": "/catchwords/static/js/runtime-main.90d493c5.js"
+  },
+  {
+    "revision": "d73c42bd888769c31504619a5f8dc3f9",
+    "url": "/catchwords/static/media/alert.d73c42bd.svg"
+  },
+  {
+    "revision": "fab2dd997752f77ee29cc21f9c326d1a",
+    "url": "/catchwords/static/media/checkmark.fab2dd99.svg"
+  }
+]);
