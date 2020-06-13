f8e2a0430265a6abb841deb5bb1bdac7a96c8db0
bstanfield <bnstnfld@gmail.com>
2020-06-13 11:20:04 -0700

    adds migration capability and csv of words for import

---
+++ backend/migrations/sqls/20200613000000-init-migration-up.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS words (
+  id SERIAL PRIMARY KEY,
+  name text,
+  votes integer DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS passwords (
+  id SERIAL PRIMARY KEY,
+  password text
+);
+
+CREATE TABLE IF NOT EXISTS boards (
+  id SERIAL PRIMARY KEY,
+  board_url text,
+  words text[],
+  player_one integer[],
+  player_two integer[],
+  timestamp date
+);
+
