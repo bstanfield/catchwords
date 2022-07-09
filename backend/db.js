62fcf69b0ed0d161ad1056aa89be23c46f8fe813
Ben Stanfield <bnstnfld@gmail.com>
2022-07-08 17:39:17 -1000

    remove comments

--- backend/db.js
+++ backend/db.js
@@ -22,45 +22,6 @@ const getWord = async (word) => db.query(`SELECT * FROM words WHERE word = ${wor
 const sqrtWordWeight = (word) => db.query(`UPDATE words SET weight = SQRT(weight) WHERE word = '${word}'`);
 
 const addWord = (word, weight = 1) => db.query(`INSERT INTO words(word, weight) VALUES('${word}', ${weight})`);
- 
-// Queries
-// const updateGame = (room, guesses, scores) => {
-//   const stringifiedGuesses = JSON.stringify(guesses);
-//   const stringifiedScores = JSON.stringify(scores);
-
-//   db.query('UPDATE rooms SET guesses = ${guesses}, scores = ${scores} WHERE room_name = ${room} AND created_at in (select max(created_at) from rooms WHERE room_name = ${room})', {
-//     room,
-//     guesses: stringifiedGuesses,
-//     scores: stringifiedScores,
-//   });
-// }
-
-// const insertCompletionTimestamp = (room, completed_at) => {
-//   db.query('UPDATE rooms SET completed_at = ${completed_at} WHERE room_name = ${room} AND created_at in (select max(created_at) from rooms WHERE room_name = ${room})', {
-//     room,
-//     completed_at,
-//   });
-// }
-
-// const getPuzzle = async (room) => db.query('SELECT * FROM rooms WHERE room_name = ${room} AND created_at in (select max(created_at) from rooms WHERE room_name = ${room})', {
-//   room,
-// });
-
-// const insertPuzzle = (created_at, completed_at, room, board, mappings, guesses, scores) => {
-//   console.log('inserting room: ', room)
-//   const stringifiedGuesses = JSON.stringify(guesses);
-//   const stringifiedScores = JSON.stringify(scores);
-
-//   db.query('INSERT INTO rooms(room_name, board, mappings, created_at, completed_at, guesses, scores) VALUES(${room}, ${board}, ${mappings}, ${created_at}, ${completed_at}, ${guesses}, ${scores})', {
-//     room,
-//     board,
-//     mappings,
-//     created_at,
-//     completed_at,
-//     guesses: stringifiedGuesses,
-//     scores: stringifiedScores,
-//   });
-// }
 
 
 module.exports = {
--- backend/endpoints/add-word.js
+++ backend/endpoints/add-word.js
@@ -3,14 +3,16 @@ const { getValidWords, addWord } = require("../db");
 exports.addWord = async (req, res) => {
   const { word, weight } = req.body;
 
+  // looks like: [{ word: 'Ben', weight: 1, id: 99 }, { ... }]
   const wordObjs = await getValidWords();
-  // i.e. { word: 'Ben', weight: 1, id: 99 }
+  
+
   const justWords = wordObjs.map(obj => obj.word);
 
   if (justWords.includes(word)) {
     return res.status(422).send('Error: word already exists in database');
   }
-  
+
   addWord(word, weight);
   res.status(200).send({ success: `word ${word} added` });
 };
