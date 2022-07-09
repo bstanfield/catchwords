0e50b5d4a2cfcf625e3c653e48e146b83db850d1
Ben Stanfield <bnstnfld@gmail.com>
2022-07-08 20:38:40 -1000

    static?

---
+++ frontend/build/precache-manifest.316799a2f52c1f047676ee4b0b98a914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8727ef6c9bb08a28f10a0e1622293e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aa22a91c85450349050",
+    "url": "/static/js/2.dbdd8897.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.dbdd8897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be35c325186fb13581f3",
+    "url": "/static/js/main.3ad98178.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.3ad98178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38345d02eb62a71ad355",
+    "url": "/static/js/runtime-main.90f3bd40.js"
+  }
+]);
